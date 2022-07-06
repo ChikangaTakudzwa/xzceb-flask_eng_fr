e76be7e0db0d0a68e0801301089c03202024e0c8
ChikangaTakudzwa <youngpainzw@gmail.com>
2022-07-06 13:44:33 +0000

    Added file at Task 2

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from translator import englishToFrench, frenchToEnglish
+
+class TestEnglish(unittest.TestCase):
+    def test_eng(self):
+
+class TestFrench(unittest.TestCase):
+    def test_fr(self):
+
+
+
+
+unittest.main()

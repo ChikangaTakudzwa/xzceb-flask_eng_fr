5651698541c1dce360cda441c9f5bd76c0093613
ChikangaTakudzwa <youngpainzw@gmail.com>
2022-07-06 13:49:49 +0000

    Added test cases

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,11 +4,13 @@ from translator import englishToFrench, frenchToEnglish
 
 class TestEnglish(unittest.TestCase):
     def test_eng(self):
+        self.assertEqual(englishToFrench(None), ' ')
+        self.assertNotEqual(englishToFrench('Hello'), 'Bonjour')
 
 class TestFrench(unittest.TestCase):
     def test_fr(self):
-
-
+        self.assertEqual(frenchToEnglish(None), ' ')
+        self.assertNotEqual(frenchToEnglish('Bonjour'), 'Hello')
 
 
 unittest.main()

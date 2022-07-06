eb43679f80fe31cfb8fe4cbac63d2b2ded309f26
ChikangaTakudzwa <youngpainzw@gmail.com>
2022-07-06 16:07:57 +0000

    Added method for en only

--- final_project/server.py
+++ final_project/server.py
@@ -5,20 +5,22 @@ import json
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+def englishToFrench(englishText):
+    textToTranslate = request.args.get(englishText)
     # Write your code here
-    return "Translated text to French"
+    txt = print(translator.english_to_french(textToTranslate))
+    return ("Translated text to French:", txt)
 
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+# @app.route("/frenchToEnglish")
+# def frenchToEnglish():
+#     textToTranslate = request.args.get('textToTranslate')
+#     # Write your code here
+#     return "Translated text to English"
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)

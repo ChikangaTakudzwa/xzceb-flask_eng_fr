4be16a8bd26c57b67e1d86ef8ccf3e62f28895ea
ChikangaTakudzwa <youngpainzw@gmail.com>
2022-07-06 13:54:05 +0000

    Added class and method comments

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,12 +3,16 @@ import unittest
 from translator import englishToFrench, frenchToEnglish
 
 class TestEnglish(unittest.TestCase):
+    """ Test en class """
     def test_eng(self):
+        """ Test en method """
         self.assertEqual(englishToFrench(None), ' ')
         self.assertNotEqual(englishToFrench('Hello'), 'Bonjour')
 
 class TestFrench(unittest.TestCase):
+    """ Test fr class """
     def test_fr(self):
+        """ Test fr method """
         self.assertEqual(frenchToEnglish(None), ' ')
         self.assertNotEqual(frenchToEnglish('Bonjour'), 'Hello')
 

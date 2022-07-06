1bd7048e285e9de3994c9e2f8e435dae525dac18
ChikangaTakudzwa <youngpainzw@gmail.com>
2022-07-06 16:13:36 +0000

    fixed err in tests py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from translator import english_to_french, french_to_english
+from . translator import english_to_french, french_to_english
 
 class TestEnglish(unittest.TestCase):
     """ Test en class """
